6e90fd183df1005e6346869f228c8a6180e5ec3a
szutciek <szutermaciek@gmail.com>
2025-06-07 21:15:54 +0200

    Updated rate limit setting

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const app = express();
 
 const rateLimit = require("express-rate-limit");
 const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
-  max: 100,
+  windowMs: 10 * 60 * 1000,
+  max: 30,
   message: "Too many requests from this IP, please try again after 15 minutes",
 });
 
