f95fa59b3a2c65e98eb7a6f7f1ab2dee7edc5b51
szutciek <szutermaciek@gmail.com>
2025-08-06 00:35:36 +0200

    Completed frontend integration

--- client/pages/sso.js
+++ client/pages/sso.js
@@ -1,4 +1,25 @@
 const url = window.location.href;
 const urlObj = new URL(url);
 let accessToken = urlObj.searchParams.get("access_token");
-console.log(accessToken);
+
+const decodeJwt = (token) => {
+  const base64Url = token.split(".")[1];
+  const base64 = base64Url.replace(/-/g, "+").replace(/_/g, "/");
+  const jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split("")
+      .map((c) => "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2))
+      .join("")
+  );
+  return JSON.parse(jsonPayload);
+};
+
+const decoded = decodeJwt(accessToken);
+
+const saveUserData = (token, user) => {
+  if (!token || !user) return;
+  window.localStorage.setItem("token", token);
+  window.localStorage.setItem("user", JSON.stringify(user));
+};
+
+saveUserData(accessToken, decoded);
